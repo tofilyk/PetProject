2acf75bc5c832e77a24e7c088ed2c0673aaeca3d
tofilyk <tofilyk@mail.ru>
2020-02-17 18:01:47 +0300

    Add Calc and menu

--- src/main/kotlin/com/github/tofilyk/petproject/Main.kt
+++ src/main/kotlin/com/github/tofilyk/petproject/Main.kt
@@ -1,9 +1,14 @@
 package com.github.tofilyk.petproject
 
+
 fun main(){
-ran()
+    println(" Enter 1 for game random \n Enter 2 for Calc \n Enter 911 for return to menu")
+    val xas = readLine()?.toInt() ?: 5
+
+    if (xas == 1) ran()
+    if (xas == 2)  allResult
+
 
 }
-
 
 
--- src/main/kotlin/com/github/tofilyk/petproject/random.kt
+++ src/main/kotlin/com/github/tofilyk/petproject/random.kt
@@ -4,21 +4,24 @@ package com.github.tofilyk.petproject
 fun ran(){
 
     println("Now you need to guess a random number. \n Choose the number of attempts from 1 to 10")
-    var answer_attempts = readLine()!!.toInt()
+    var answer_attempts = readLine()?.toInt() ?: 0
     val win_limit = answer_attempts * 2
     when (answer_attempts) {
         in (1..10) -> println("Choose a number between $win_limit-100")
-        !in (1..10) -> {
+        911 -> return main()
+        else -> {
             println("Invalid number")
-            return
-        }
+             return ran()
+                    }
+
     }
     val answer = readLine()!!.toInt()
     when (answer) {
         in (win_limit..100) -> println("Now enter the number.I will help with tips. \n Enter the number.")
-        !in (win_limit..100) -> {
+        911 -> return main()
+        else -> {
             println("Invalid number")
-            return
+            return ran()
         }
     }
     val result = answer_attempts.toDouble() / answer.toDouble() * 100
@@ -26,15 +29,18 @@ fun ran(){
     val random_number = (1..answer).random()
     for (it in 1..answer_attempts) {
         val answer_final = readLine()!!.toInt()
+        if (answer_final == 911) return main()
         if ("$answer_final" == "$random_number") {
             println("True")
-            return
+            return main()
         }
         if ("$answer_final" > "$random_number") println("Select a lower number")
         if ("$answer_final" < "$random_number") println("Select a larger number")
         val attempts_left = (answer_attempts-- - 1)
         println("Attempts_left $attempts_left")
     }
+
+    return main()
 }
 
 
