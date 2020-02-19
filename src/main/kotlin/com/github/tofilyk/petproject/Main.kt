db534b6d92881d95b7779824ae6e720770d180c2
tofilyk <tofilyk@mail.ru>
2020-02-19 23:54:18 +0300

    Bubble up added

--- src/main/kotlin/com/github/tofilyk/petproject/Main.kt
+++ src/main/kotlin/com/github/tofilyk/petproject/Main.kt
@@ -1,13 +1,18 @@
 package com.github.tofilyk.petproject
 
 
-fun main(){
-    println(" Enter 1 for game random \n Enter 2 for Calc \n Enter 3 for Salary \n Enter 911 for return to menu")
+fun main() {
+    println(
+        " Enter 1 for game random \n Enter 2 for Calc \n Enter 3 for Salary \n" +
+                " Enter 4 for bubble sort \n Enter 911 for return to menu "
+    )
     val xas = readLine()?.toInt() ?: 5
 
     if (xas == 1) ran()
-    if (xas == 2)  allResult
-    if (xas == 3)  salary()
+    if (xas == 2) allResult
+    if (xas == 3) salary()
+    if (xas == 4) bubble()
+
 
 }
 
