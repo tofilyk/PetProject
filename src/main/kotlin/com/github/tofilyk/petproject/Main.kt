3e0757f54dab0db518ebd0da59346bd8159395e0
tofilyk <tofilyk@mail.ru>
2020-02-29 19:42:16 +0300

    Commiting with wrong formatting to check precommit hook

--- src/main/kotlin/com/github/tofilyk/petproject/Main.kt
+++ src/main/kotlin/com/github/tofilyk/petproject/Main.kt
@@ -7,7 +7,7 @@ fun main() {
                 " Enter 1 for Play Guess the number \n Enter 2 for Mortgage calculator \n Enter 3 for Salary calculator \n" +
                 " Enter 4 for Bubble sort \n Enter 911 for return to menu "
     )
-    val xas = readLine()?.toInt()
+    val xas =         readLine()?.toInt()
 
     if (xas == 1) ran()
     if (xas == 2) allres
