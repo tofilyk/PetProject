e65fac87e1399755fa6328623a446ca7c06e1e0a
tofilyk <tofilyk@mail.ru>
2020-03-04 19:34:29 +0300

    Add Triangle

--- src/main/kotlin/com/github/tofilyk/petproject/Main.kt
+++ src/main/kotlin/com/github/tofilyk/petproject/Main.kt
@@ -1,18 +1,21 @@
 package com.github.tofilyk.petproject
 
 fun main() {
+
     println(
         "\n" +
                 "      ======MENU======" + "\n" +
                 " Enter 1 for Play Guess the number \n Enter 2 for Mortgage calculator \n Enter 3 for Salary calculator \n" +
-                " Enter 4 for Bubble sort \n Enter 911 for return to menu "
+                " Enter 4 for Bubble sort \n Enter 5 for Triangle \n Enter 911 for return to menu"
     )
-    val xas =         readLine()?.toInt()
+    val xas = readLine()?.toInt()
 
     if (xas == 1) ran()
     if (xas == 2) allres
     if (xas == 3) salary()
     if (xas == 4) bubble()
+    if (xas == 5) triangle()
 
     if (xas == 911) main()
+
 }
---
+++ src/main/kotlin/com/github/tofilyk/petproject/Triangle.kt
@@ -0,0 +1,39 @@
+package com.github.tofilyk.petproject
+
+fun triangle() {
+
+
+    println("Enter side A")
+    val textA = readLine()?.toInt() ?: 1
+    println("Enter side B")
+    val textB = readLine()?.toInt() ?: 1
+    println("Enter side C")
+    val textC = readLine()?.toInt() ?: 1
+
+    if (textA == 0 || textB == 0 || textC == 0)
+        println("Side cannot be zero")
+
+
+
+
+      else if (textA > textB + textC) println("Cannot be a triangle")
+     else  if (textB > textA + textC) println("Cannot be a triangle")
+     else  if (textC > textA + textB) println("Cannot be a triangle")
+
+
+
+   else  if (textA == textB && textA == textC && textB == textC)
+        println("This is an equilateral triangle.")
+
+   else  if (textA == textB && textA != textC && textB != textC)
+        println("This is an isosceles triangle.")
+   else  if (textA == textC && textA != textB && textC != textB)
+        println("This is an isosceles triangle.")
+    else if (textC == textB && textC != textA && textB != textA)
+        println("This is an isosceles triangle.")
+
+   else  if (textA != textB && textB!= textC && textA != textC)
+        println("This is a scalene triangle.")
+
+    return main()
+}
