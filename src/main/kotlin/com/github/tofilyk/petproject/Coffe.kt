4b5fe0c679d06b3bf226275560dba9373a07682d
tofilyk <tofilyk@mail.ru>
2020-03-11 17:04:46 +0300

    Update Coffe machine Project

--- src/main/kotlin/com/github/tofilyk/petproject/Coffe.kt
+++ src/main/kotlin/com/github/tofilyk/petproject/Coffe.kt
@@ -4,100 +4,79 @@ import java.util.Scanner
 
 fun coffe() {
     val scanner = Scanner(System.`in`)
-    val water = 1200
-    val milk = 540
-    val coffe = 120
-    val cups = 9
-    val money = 550
-
-    fun result() {
-        println(
+    var water = 400
+    var milk = 540
+    var coffe = 120
+    var cups = 9
+    var money = 550
+    repeat(500) {
+        fun printMenu() = println("Write action (buy, fill, take, remaining, exit):")
+        printMenu()
+        fun result() = println(
             "The coffee machine has:\n" +
                     "$water of water\n" +
                     "$milk of milk\n" +
                     "$coffe of coffee beans\n" +
                     "$cups of disposable cups\n" +
-                    "$money of money\n \n" +
-                    "Write action (buy, fill, take):"
+                    "$money of money\n \n"
         )
-    }
+        val userInput = scanner.next()
 
-    fun cappuccino() {
-        println(
-            "The coffee machine has:\n" +
-                    "${water - 200} of water\n" +
-                    "${milk - 100} of milk\n" +
-                    "${coffe - 12} of coffee beans\n" +
-                    "${cups - 1} of disposable cups\n" +
-                    "${money + 6} of money\n \n"
-        )
-    }
+        if (userInput == "buy") {
+            println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
 
-    fun espresso() {
-        println(
-            "The coffee machine has:\n" +
-                    "${water - 250} of water\n" +
-                    "${milk - 0} of milk\n" +
-                    "${coffe - 16} of coffee beans\n" +
-                    "${cups - 1} of disposable cups\n" +
-                    "${money + 4} of money\n \n"
-        )
-    }
+            if (water > 350 && milk > 100 && coffe > 20 && cups > 1)
+                println("I have enough resources, making you a coffee!")
+            if (water < 350 || milk < 100 || coffe < 20 || cups < 1) {
+                println("Sorry, not enough water!")
+                return@repeat
+            }
 
-    fun latte() {
-        println(
-            "The coffee machine has:\n" +
-                    "${water - 350} of water\n" +
-                    "${milk - 75} of milk\n" +
-                    "${coffe - 20} of coffee beans\n" +
-                    "${cups - 1} of disposable cups\n" +
-                    "${money + 7} of money\n \n"
-        )
-    }
+            val userInput2 = scanner.next()
+            if (userInput2 == "back")
+                return@repeat
 
-    result()
-    val userInput = scanner.next()
-    if (userInput == "buy") {
-        println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
-        val userInput2 = scanner.nextInt()
-        if (userInput2 == 1) espresso()
-        if (userInput2 == 2) latte()
-        if (userInput2 == 3) cappuccino()
-    }
-
-    if (userInput == "fill") {
-        println("Write how many ml of water do you want to add:")
-        val userFill1 = scanner.nextInt()
-        println("Write how many ml of milk do you want to add:")
-        val userFill2 = scanner.nextInt()
-        println("Write how many grams of coffee beans do you want to add:")
-        val userFill3 = scanner.nextInt()
-        println("Write how many disposable cups of coffee do you want to add:")
-        val userFill4 = scanner.nextInt()
-        fun fill() {
-            println(
-                "The coffee machine has:\n" +
-                        "${water + userFill1} of water\n" +
-                        "${milk + userFill2} of milk\n" +
-                        "${coffe + userFill3} of coffee beans\n" +
-                        "${cups + userFill4} of disposable cups\n" +
-                        "$money of money\n \n"
-            )
+            if (userInput2 == "1") {
+                water -= 250
+                coffe -= 16
+                cups -= 1
+                money += 4
+            }
+            if (userInput2 == "2") {
+                water -= 350
+                milk -= 75
+                coffe -= 20
+                cups -= 1
+                money += 7
+            }
+            if (userInput2 == "3") {
+                water -= 200
+                milk -= 100
+                coffe -= 12
+                cups -= 1
+                money += 6
+            }
         }
-        fill()
-    }
-    if (userInput == "take") {
-        println("I gave you $$money \n")
-        fun take() {
-            println(
-                "The coffee machine has:\n" +
-                        "$water of water\n" +
-                        "$milk of milk\n" +
-                        "$coffe of coffee beans\n" +
-                        "$cups of disposable cups\n" +
-                        "0 of money\n \n"
-            )
+        if (userInput == "fill") {
+            println("Write how many ml of water do you want to add:")
+            val userFill1 = scanner.nextInt()
+            water += userFill1
+            println("Write how many ml of milk do you want to add:")
+            val userFill2 = scanner.nextInt()
+            milk += userFill2
+            println("Write how many grams of coffee beans do you want to add:")
+            val userFill3 = scanner.nextInt()
+            coffe += userFill3
+            println("Write how many disposable cups of coffee do you want to add:")
+            val userFill4 = scanner.nextInt()
+            cups += userFill4
         }
-        take()
+        if (userInput == "take") {
+            println("I gave you $$money \n")
+            money = 0
+        }
+        if (userInput == "remaining") result()
+        if (userInput == "exit") return
+
     }
 }
